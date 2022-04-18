63f6e46378214f78689f8c3f36dd4afa684fdba5
Nikolaos Dimopoulos <nikos@niden.net>
2022-04-18 13:38:51 -0400

    corrected interface types with some refactoring

--- src/Html/Link/Link.php
+++ src/Html/Link/Link.php
@@ -22,4 +22,59 @@ use Psr\Link\LinkInterface;
  */
 class Link extends AbstractLink implements LinkInterface
 {
+    /**
+     * Returns a list of attributes that describe the target URI.
+     *
+     * @return array
+     *   A key-value list of attributes, where the key is a string and the value
+     *  is either a PHP primitive or an array of PHP strings. If no values are
+     *  found an empty array MUST be returned.
+     */
+    public function getAttributes()
+    {
+        return $this->doGetAttributes();
+    }
+
+    /**
+     * Returns the target of the link.
+     *
+     * The target link must be one of:
+     * - An absolute URI, as defined by RFC 5988.
+     * - A relative URI, as defined by RFC 5988. The base of the relative link
+     *     is assumed to be known based on context by the client.
+     * - A URI template as defined by RFC 6570.
+     *
+     * If a URI template is returned, isTemplated() MUST return True.
+     *
+     * @return string
+     */
+    public function getHref()
+    {
+        return $this->doGetHref();
+    }
+
+    /**
+     * Returns the relationship type(s) of the link.
+     *
+     * This method returns 0 or more relationship types for a link, expressed
+     * as an array of strings.
+     *
+     * @return string[]
+     */
+    public function getRels()
+    {
+        return $this->doGetRels();
+    }
+
+    /**
+     * Returns whether this is a templated link.
+     *
+     * @return bool
+     *   True if this link object is templated, False otherwise.
+     */
+    public function isTemplated()
+    {
+        return $this->doIsTemplated();
+    }
+
 }
